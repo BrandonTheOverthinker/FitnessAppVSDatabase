c5d93c07d9c7f557cf4073d5838cf1db6ad834d5
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-02 14:44:16 +0000

    Implement Macronutrients Table

---
+++ NEAFitness/Macronutrients.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Macronutrients]
+(
+	[MacroId] INT NOT NULL PRIMARY KEY,
+	[FoodId] INT NOT NULL,
+	[Calories] INT NOT NULL,
+	[Protein] DECIMAL(7,2) NOT NULL,
+	[Fat] DECIMAL(7,2) NOT NULL,
+	[SaturatedFat] DECIMAL(7,2) NOT NULL,
+	[Carbohydrates] DECIMAL(7,2) NOT NULL,
+	[Sugar] DECIMAL(7,2) NOT NULL,
+	[Fibre] DECIMAL(7,2) NOT NULL,
+
+	CONSTRAINT [FK_Macronutrients_Food_FoodId] FOREIGN KEY (FoodId) REFERENCES [dbo].[Food](FoodId)
+)
