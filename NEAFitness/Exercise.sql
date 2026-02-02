27537293bbf90de43e88fd98904711a3d72fd358
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-02 14:03:19 +0000

    fixed foreign key declaration syntax

--- NEAFitness/Exercise.sql
+++ NEAFitness/Exercise.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[Exercise]
 (
     [ExerciseID] INT NOT NULL PRIMARY KEY,
     [UserID] INT NOT NULL,
-    [CONSTRAINT] [FK_ExerciseInfo_User_UserID] FOREIGN KEY REFERENCES [dbo].[User](UserID),
+    CONSTRAINT [FK_ExerciseInfo_User_UserID] FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
     [ExerciseName] NVARCHAR(50) NOT NULL,
     [ExerciseType] NVARCHAR(20) NOT NULL,
 )
--- NEAFitness/UserExercise.sql
+++ NEAFitness/UserExercise.sql
@@ -5,6 +5,6 @@ CREATE TABLE [dbo].[UserExercise]
     [DefaultRestSeconds] INT,
     [Notes] NVARCHAR(500) NULL,
     CONSTRAINT [PK_UserExercise] PRIMARY KEY (UserID, ExerciseID),
-    CONSTRAINT [FK_UserExercise_User_UserID] FOREIGN KEY REFERENCES [dbo].[User](UserID),
-    CONSTRAINT [FK_UserExercise_Exercise_ExerciseID] FOREIGN KEY REFERENCES [dbo].[Exercise](ExerciseID)
+    CONSTRAINT [FK_UserExercise_User_UserID] FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
+    CONSTRAINT [FK_UserExercise_Exercise_ExerciseID] FOREIGN KEY (ExerciseID) REFERENCES [dbo].[Exercise](ExerciseID)
 )
