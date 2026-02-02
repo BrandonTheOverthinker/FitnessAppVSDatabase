15cf6b205d18b30b36de04707d1e8e5f7b361597
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-02 14:35:54 +0000

    Changed foreign key name and added unique constraint

--- NEAFitness/Exercise.sql
+++ NEAFitness/Exercise.sql
@@ -2,7 +2,10 @@ CREATE TABLE [dbo].[Exercise]
 (
     [ExerciseID] INT NOT NULL PRIMARY KEY,
     [UserID] INT NOT NULL,
-    CONSTRAINT [FK_ExerciseInfo_User_UserID] FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
     [ExerciseName] NVARCHAR(50) NOT NULL,
     [ExerciseType] NVARCHAR(20) NOT NULL,
+
+    CONSTRAINT [FK_Exercise_User_UserID] FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
+
+    CONSTRAINT [UQ_Exercise_ExerciseName] UNIQUE (ExerciseName)
 )
