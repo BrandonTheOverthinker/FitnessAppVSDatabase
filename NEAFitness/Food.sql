9a984abd89b87184403a589f8f53ec048dcf8eca
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-02 14:40:25 +0000

    Implement Food Table

---
+++ NEAFitness/Food.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Food]
+(
+	[FoodId] INT NOT NULL PRIMARY KEY,
+	[UserID] INT NOT NULL,
+	[FoodName] NVARCHAR(50) NOT NULL,
+
+	CONSTRAINT [FK_Food_User_UserID] FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
+)
