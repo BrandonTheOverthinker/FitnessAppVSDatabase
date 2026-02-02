fdf8aa4e76b2771fdd40e962d40262176093528a
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-02 14:42:16 +0000

    Implement FoodLog Table

---
+++ NEAFitness/FoodLog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[FoodLog]
+(
+	[FoodLogId] INT NOT NULL PRIMARY KEY,
+	[FoodId] INT NOT NULL,
+	[LogTime] DATETIME NOT NULL,
+	[Quantity] DECIMAL(7,2) NOT NULL,
+
+	CONSTRAINT [FK_FoodLog_Food_FoodId] FOREIGN KEY (FoodId) REFERENCES [dbo].[Food](FoodId)
+)
