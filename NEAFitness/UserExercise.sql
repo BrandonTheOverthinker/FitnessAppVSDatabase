98d1310daed08613e07e91a47014be9520fe619e
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-05 00:43:36 +0000

    Used UserExercise table again and removed 2 table elements

--- NEAFitness/UserExercise.sql
+++ NEAFitness/UserExercise.sql
@@ -1,14 +1,9 @@
-CREATE TABLE [dbo].[UserExercise]
+﻿CREATE TABLE [dbo].[UserExercise]
 (
-    [UserID] INT NOT NULL,
-    [ExerciseID] INT NOT NULL,
-    [DefaultRestSeconds] INT,
-    [Notes] NVARCHAR(500) NULL,
+	[UserId] INT NOT NULL,
+	[ExerciseID] INT NOT NULL,
 
-    CONSTRAINT [PK_UserExercise] PRIMARY KEY (UserID, ExerciseID), -- Composite Key
-
-    CONSTRAINT [FK_UserExercise_User_UserID] FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
-    CONSTRAINT [FK_UserExercise_Exercise_ExerciseID] FOREIGN KEY (ExerciseID) REFERENCES [dbo].[Exercise](ExerciseID),
-
-    CONSTRAINT [CHK_UserExercise_DefaultRestSeconds] CHECK (DefaultRestSeconds IS NULL OR DefaultRestSeconds >= 0)
+	CONSTRAINT [PK_UserExercise] PRIMARY KEY (UserId, ExerciseID),
+	CONSTRAINT [FK_UserExercise_User_UserID] FOREIGN KEY (UserId) REFERENCES [dbo].[User](UserId),
+	CONSTRAINT [FK_UserExercise_Exercise_ExerciseID] FOREIGN KEY (ExerciseID) REFERENCES [dbo].[Exercise](ExerciseID)
 )
