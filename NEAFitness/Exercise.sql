9be5aedc4de9af230913c27933250cf5f0f21f54
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-05 00:31:55 +0000

    Removed UserID FK

--- NEAFitness/Exercise.sql
+++ NEAFitness/Exercise.sql
@@ -1,11 +1,8 @@
 CREATE TABLE [dbo].[Exercise]
 (
     [ExerciseID] INT NOT NULL PRIMARY KEY,
-    [UserID] INT NOT NULL,
     [ExerciseName] NVARCHAR(50) NOT NULL,
     [ExerciseType] NVARCHAR(20) NOT NULL,
-
-    CONSTRAINT [FK_Exercise_User_UserID] FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
 
     CONSTRAINT [UQ_Exercise_ExerciseName] UNIQUE (ExerciseName)
 )
