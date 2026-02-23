86722f1613800d0c1a6b2bcf24003bfaf427b8eb
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-23 16:58:26 +0000

    Fixed FoodItem SQL code

--- NEAFitness/FoodItem.sql
+++ NEAFitness/FoodItem.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[FoodItem]
 (
 	[FoodItemId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	[FoodName] NVARCHAR(50) NOT NULL,
+	[FoodName] NVARCHAR(100) NOT NULL,
 	[Barcode] NVARCHAR(50) NULL,
 
-	[Calories] DECIMAL(7,2) NOT NULL,
+	[Calories] INT NOT NULL,
 	[Protein] DECIMAL(7,2) NOT NULL,
 	[Fat] DECIMAL(7,2) NOT NULL,
 	[SaturatedFat] DECIMAL(7,2) NOT NULL,
@@ -12,7 +12,9 @@
 	[Sugar] DECIMAL(7,2) NOT NULL,
 	[Fibre] DECIMAL(7,2) NOT NULL,
 
-	[CreatedByUserID] INT NOT NULL,
+	[CreatedByUserID] INT NULL, -- public if null
 
 	CONSTRAINT [FK_FoodItem_User_UserID] FOREIGN KEY (CreatedByUserID) REFERENCES [dbo].[User](UserID),
+
+
 )
