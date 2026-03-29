d2a44ef2796ddb288d86e77485c58d375e3b70bf
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-03-29 17:32:29 +0100

    add target distance to exercise goal db

--- NEAFitness/ExerciseGoal.sql
+++ NEAFitness/ExerciseGoal.sql
@@ -1,11 +1,12 @@
-﻿CREATE TABLE [dbo].[ExerciseGoal]
-(
-	[EGoalId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	[GoalID] INT NOT NULL,
-	[ExerciseID] INT NOT NULL,
-	[TargetWeight] DECIMAL(7,2) NOT NULL,
-	[TargetTime] INT NOT NULL,
+﻿CREATE TABLE [dbo].[ExerciseGoal] (
+    [EGoalId]      INT            IDENTITY (1, 1) NOT NULL,
+    [GoalID]       INT            NOT NULL,
+    [ExerciseID]   INT            NOT NULL,
+    [TargetWeight] DECIMAL (7, 2) NOT NULL,
+    [TargetTime]   INT            NOT NULL,
+    [TargetDistance] DECIMAL (7, 2) NOT NULL, 
+    PRIMARY KEY CLUSTERED ([EGoalId] ASC),
+    CONSTRAINT [FK_ExerciseGoal_Exercise_ExerciseID] FOREIGN KEY ([ExerciseID]) REFERENCES [dbo].[Exercise] ([ExerciseID]),
+    CONSTRAINT [FK_ExerciseGoal_Goal_GoalID] FOREIGN KEY ([GoalID]) REFERENCES [dbo].[UserGoal] ([GoalId])
+);
 
-	CONSTRAINT [FK_ExerciseGoal_Goal_GoalID] FOREIGN KEY (GoalID) REFERENCES [dbo].[UserGoal](GoalID),
-	CONSTRAINT [FK_ExerciseGoal_Exercise_ExerciseID] FOREIGN KEY (ExerciseID) REFERENCES [dbo].[Exercise](ExerciseID)
-)
